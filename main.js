82ee95aea105a89b69ee55868d9e99c4f5c0be4a
raghavendrasangam <raghavendrasangam0007@gmail.com>
2023-08-29 22:55:06 +0530

    game_completed

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-var timer = 10;
+var timer = 60;
+var score= 0;
+var rn=0;
+//event bubbling---when clickedon something event will raise, 
+
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
 
 function getNewHit(){
-    var rn = Math.floor(Math.random()*10);
+    rn = Math.floor(Math.random()*10);
     document.querySelector("#hitval").textContent = rn;
+
 }
 
 function makeBubble(){
@@ -23,10 +33,25 @@ function runTimer(){
     }
     else{
         clearInterval(timerint);
+        document.querySelector("#pbtm").innerHTML=`<h1 class="over">GAME OVER</h1><br>
+        <br><h1 id="highscore">YOUR SCORE = ${score}</h1>`
     }
     }, 1000)
 }
 
+document.querySelector("#pbtm").addEventListener("click", function(details){
+    var clickednum = Number(details.target.textContent);
+
+    if(clickednum == rn){
+        increaseScore(); // increses score when clicked on same num
+        makeBubble(); //new bubbles formed
+        getNewHit(); // new hit number
+    }
+});
+
+
+
 getNewHit();
 runTimer();
 makeBubble();
+
